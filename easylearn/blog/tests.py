b225cc06a5206c0c8d6143e3fde03e039c52b8ca
Yazan Kabbant <yazanKabbany@gmail.com>
2018-03-09 13:01:12 +0200

    update blog tests

--- easylearn/blog/tests.py
+++ easylearn/blog/tests.py
@@ -1,37 +1,43 @@
 from django.test import TestCase
 
-from .models import BlogPost
+from blog.models import BlogPost
 from easylearn.users.models import User
 
 # Create your tests here.
 
 class BlogPostModelTest(TestCase):
+
     def setUp(self):
         self.user = User.objects.create_user('yazan')
-        self.user2 = User.objects.create_user('habib')        
+        self.user2 = User.objects.create_user('habib')
+        self.post = BlogPost.objects.create(title='some title', 
+                                        text='some text', writer=self.user)  
     def test_unique_slug(self):
         """
         two post with identical titles should different slugs
         """
-        repeated_title = "good blog"
-        post = BlogPost()
-        post.title = repeated_title
-        post.writer = self.user
-        post.text = "some radom text"
-        post.save()
-
+        repeated_title = self.post.title
+        
         post2 = BlogPost()
         post2.title = repeated_title
         post2.writer = self.user2
         post2.text = "another random text"
         post2.save()
 
-        self.assertNotEqual(post.slug, post2.slug)
+        self.assertNotEqual(self.post.slug, post2.slug)
     
     def test_has_slug(self):
         """
         post slug is not an empty string
         """
-        post = BlogPost.objects.create(title='some title', 
-                                        text='some text', writer=self.user)
-        self.assertNotEqual(post.slug, '')
+        self.assertNotEqual(self.post.slug, '')
+    
+    def test_slug_dont_change(self):
+        """
+        post slug is not changed
+        """
+        previus_slug = self.post.slug
+        self.post.title = "Changed title"
+        self.post.save()
+        self.assertEqual(previus_slug, self.post.slug)
+        
