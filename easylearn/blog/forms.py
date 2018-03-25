55ff65047a490cd916152dcf0d75d680e96f0055
yazan kabbany <yazanKabbany@gmail.com>
2018-03-25 09:39:43 +0200

    add rtl support to "write form"

--- easylearn/blog/forms.py
+++ easylearn/blog/forms.py
@@ -4,8 +4,10 @@ from blog.models import BlogPost
 
 class BlogPostForm(forms.ModelForm):
     """BlogPostForm definition."""
-    text = forms.CharField(widget=forms.Textarea(attrs={'cols':100}), 
-    max_length=4000, required=True)
+    text = forms.CharField(widget=forms.Textarea(attrs={'cols':100, 'class':'arabic-text'}), 
+    max_length=4000, required=True, label='المنشور')
+    title = forms.CharField(widget = forms.TextInput(attrs={'cols':50, 'class':'arabic-text'}),
+     max_length=50, required=True, label='العنوان')
 
     class Meta:
 
